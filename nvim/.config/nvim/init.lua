3bf36fd0231f6beaadc41cfcd94353a268d35e18
Jaša Marolt <jasa.marolt@gmail.com>
2025-08-29 17:36:35 +0200

    before ecosse3 nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -13,3 +13,23 @@ vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
 require("lazy").setup("plugins")
+require('lspconfig').glslls.setup{}
+vim.api.nvim_create_user_command(
+  'Run',
+  '!ninja -C build && ./build/main',
+  {}
+)
+
+vim.api.nvim_set_keymap(
+  'n',
+  '<F5>',
+  ':Run<CR>',
+  { noremap = true, silent = true }
+)
+return {
+  'neovim/nvim-lspconfig', -- Required to configure LSP servers
+  'williamboman/mason.nvim', -- Installs and manages language servers
+  'williamboman/mason-lspconfig.nvim', -- Bridges mason and nvim-lspconfig
+  'hrsh7th/nvim-cmp', -- Autocompletion engine
+  'hrsh7th/cmp-nvim-lsp', -- Autocompletion source for LSP
+}
