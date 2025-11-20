f157e10e203935634509e074cf0aa638f525bff3
Jaša Marolt <jasa.marolt@gmail.com>
2025-11-20 12:06:13 +0100

    fonts, neovim image

--- bin/.local/bin/seedMeadow.sh
+++ bin/.local/bin/seedMeadow.sh
@@ -18,10 +18,22 @@ sudo pacman -S --noconfirm \
   navi \
   tldr \
   impala \
-  openssh
+  openssh \
+  vlc \
+  wl-clipboard \
+  nerd-fonts
 
 echo "Installation complete for official repository packages."
 echo ""
 echo "Installing from yay"
 
-yay -S zoxide
+yay -S \
+  zoxide
+#visual-studio-code-bin
+
+if command -v fish &>/dev/null; then
+  chsh -s $(which fish)
+  echo "shell changed to fish, log out and back in to apply change"
+else
+  echo "error fish not installed"
+fi
